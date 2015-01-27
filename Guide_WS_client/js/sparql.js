f5298b6e8e1687f8b545a39a2b76d399a6d8b22d
Ahmed Missaoui <ahmed.missaoui.88@gmail.com>
2015-01-27 17:10:53 +0100

    Client update

--- Guide_WS_client/js/sparql.js
+++ Guide_WS_client/js/sparql.js
@@ -1,31 +1,52 @@
-	function listeEpicUS (){
-		  // Empty content string
-		  var tableContent = '';
-		  tableContent +='<thead><tr> <br/>\
-			<td>Name</td><td>Actions</td>\
-			</tr></thead>';;
-			tableContent += '<tbody>'
-			
-			<!-- rows will go here -->
-		  var url = "http://localhost:3030/data/query";
-		  //il ne faut pas mettre le caractere # , mais %23 a la place
-		  
-		  var query = "\PREFIX tourisme: <http://www.polytech.semantique/tourisme%23> "+
+ function getALLData (){
+		 var tableContent ='<thead><tr> <br/> <td>x</td><td>y</td><td>z</td></tr></thead> <tbody>';
+		 var url = "http://localhost:3030/data/query";		 
+		 var query = "\PREFIX tourisme: <http://www.polytech.semantique/tourisme%23> "+
 		 " SELECT * "+
-		 " WHERE { "+
-		 " ?x tourisme:dans tourisme:Rome;"+
-		 "}";
+		 " WHERE {  ?x ?p ?y }";
 		 
-		//var queryUrl = encodeURI( url+"?query="+query+"&format=json" );
 		var queryUrl = url+"?query="+query+"&format=json" ;
 
 		 $.ajax({
-		dataType: "jsonp",
-		url: queryUrl,
-			success: function( _data ) {
+				dataType: "jsonp",
+				url: queryUrl,
+				success: function( _data ) {
 				var results = _data.results.bindings;
 				sparqlItemsData = _data;
-				alert("hello");
-			});	
-		}
-	}
+				   for ( var i in results ) {
+                        tableContent += '<tr>';
+                        tableContent += '<td>' + results[i].x.value + '</td>';
+						tableContent += '<td>' + results[i].p.value + '</td>';
+						tableContent += '<td>' + results[i].y.value + '</td>';
+                        tableContent += '</tr>'
+                    }
+				$("#res").html(tableContent);
+			}	
+		});
+}
+
+
+ function getDataVille (ville){
+		 var tableContent ='<thead><tr> <br/> <td>x</td></tr></thead> <tbody>';
+		 var url = "http://localhost:3030/data/query";		 
+		 var query = "\PREFIX tourisme: <http://www.polytech.semantique/tourisme%23> "+
+		 " SELECT * "+
+		 " WHERE {  ?x tourisme:dans tourisme:"+ville+" }";
+		 
+		var queryUrl = url+"?query="+query+"&format=json" ;
+
+		 $.ajax({
+				dataType: "jsonp",
+				url: queryUrl,
+				success: function( _data ) {
+				var results = _data.results.bindings;
+				sparqlItemsData = _data;
+				   for ( var i in results ) {
+                        tableContent += '<tr>';
+                        tableContent += '<td>' + results[i].x.value + '</td>';						
+                        tableContent += '</tr>'
+                    }
+				$("#res").html(tableContent);
+			}	
+		});
+}
