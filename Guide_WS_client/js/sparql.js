99ad4964cbaad7308d88f7544628ca520cd34fc8
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2015-02-04 21:46:31 +0100

    monument at reel time DONE

--- Guide_WS_client/js/sparql.js
+++ Guide_WS_client/js/sparql.js
@@ -941,3 +941,69 @@ function getRecommendation (uri){
 			}	
 		});
 }
+
+
+
+
+/* Ayoub function return Monuments in reel time*/
+ function getReelMonuments (ville){
+		 var tableContent ='<thead><tr> <br/> <td>x</td></tr></thead> <tbody>';
+		 //var url = "http://localhost:3030/data/query";		 
+ var url = "http://localhost:8080/sparql/template?profile=st%3Aldp&query=";
+		 //var query = "\PREFIX tourisme: <http://www.polytech.semantique/tourisme%23> "+
+		 var query = "PREFIX dbo:<http://dbpedia.org/ontology/>PREFIX : <http://dbpedia.org/resource/>PREFIX foaf:<http://xmlns.com/foaf/0.1/>PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema%23>PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns%23>PREFIX dbpprop: <http://dbpedia.org/property/>PREFIX grs:<http://www.georss.org/georss/>SELECT ?description ?imageLink ?website WHERE { service <http://dbpedia.org/sparql> { {?x rdf:type dbo:Museum;dbo:abstract ?description;dbo:thumbnail ?imageLink;dbpprop:pushpinMap '"+ville+"'@en;dbpprop:website ?website;grs:point ?localisation.FILTER(langMatches(lang(?description), 'FR'))} UNION {?x rdf:type dbo:Museum;dbo:abstract ?description;dbo:thumbnail ?imageLink;dbo:location '"+ville+"'@en;dbpprop:website ?website;grs:point ?localisation.FILTER(langMatches(lang(?description), 'FR'))}}}";
+		 
+		 //alert(query);
+		 //http://localhost:8080/sparql/template?profile=st%3Aldp&query=PREFIX+tourisme%3A+%3Chttp%3A%2F%2Fwww.polytech.semantique%2Ftourisme%23%3E+%0D%0ASELECT+*+WHERE+%7B%0D%0A++%3Fx+tourisme%3Adans+tourisme%3ANice%0D%0A+%7D
+		 
+		 
+		//var queryUrl = url+"?query="+query+"&format=json" ;
+	var queryUrl = url+query ;
+
+		 $.ajax({
+				//dataType: "jsonp",
+				url: queryUrl,
+				success: function( _data ) {
+					//alert(_data.substring(_data.lastIndexOf("<table>"),_data.lastIndexOf("</table>")+8));
+					/*
+				var results = _data.results.bindings;
+				sparqlItemsData = _data;
+				   for ( var i in results ) {
+                        tableContent += '<tr>';
+                        tableContent += '<td>' + results[i].x.value + '</td>';						
+                        tableContent += '</tr>'
+                    }
+				$("#res").html(tableContent);
+				
+				*/
+				var table=_data.substring(_data.lastIndexOf("<table>"),_data.lastIndexOf("</table>")+8);
+				/*
+				var re = new RegExp('<td>.*</td>');
+				var results  = re.exec(table);
+				for (int i=0;i<results.length;i++) ) {
+						alert(results[i]);
+							//result.next;
+				}
+				*/
+				var el=document.createElement('div');
+				el.innerHTML=table;
+				var elements=el.getElementsByTagName('td');
+				
+				/*for(var i=2;i<elements.length;i++){	
+					var url= elements[i].innerHTML;
+					var toto="<img src="+url+"/>";
+					elements[i].innerHTML=toto;
+					i++;
+				}*/
+				/* Script enlevé les guillemets */
+				var reg=new RegExp('["]+', 'g');
+				for(var i=1;i<elements.length;i++){	
+					var desc= elements[i].innerHTML;
+					desc=desc.replace(reg, ''); 
+					elements[i].innerHTML=desc;
+				}
+				/*  */
+				$("#res").html(el);
+			}	
+		});
+}
